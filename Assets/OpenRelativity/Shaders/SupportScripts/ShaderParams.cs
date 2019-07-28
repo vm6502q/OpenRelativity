40e46bcbffed2290326d5dc37365c9f037967844
Daniel Strano <stranoj@gmail.com>
2019-07-28 19:04:38 -0400

    Debugging mesh collider shader

--- Assets/OpenRelativity/Shaders/SupportScripts/ShaderParams.cs
+++ Assets/OpenRelativity/Shaders/SupportScripts/ShaderParams.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 namespace OpenRelativity
 {
     //Shader properties:
-    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 424)]
+    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 352)]
     public struct ShaderParams
     {
         //[FieldOffset(0)]
-        public Matrix4x4 ltwMatrix; //local-to-world transform matrix
+        public Matrix4x4 ltwMatrix; //local to world matrix of transform
         //[FieldOffset(16)]
-        public Matrix4x4 wtlMatrix; //world-to-local transform matrix
+        public Matrix4x4 wtlMatrix; //world to local matrix of transform
         //[FieldOffset(32)]
-        public Matrix4x4 vpcLorentzMatrix; //Lorentz transform between world and player
-        //[FieldOffset(64)]
-        public Matrix4x4 viwLorentzMatrix; //Lorentz transform between world and object
-        //[FieldOffset(80)]
-        public Vector4 viw; //velocity of object in synchronous coordinates
-        //[FieldOffset(84)]
+        public Vector4 viw; //velocity of object in world
+        //[FieldOffset(36)]
         public Vector4 vpc; //velocity of player
-        //[FieldOffset(88)]
+        //[FieldOffset(40)]
         public Vector4 playerOffset; //player position in world
-        //[FieldOffset(92)]
-        public Vector4 pap; //acceleration of player in world coordinates
-        //[FieldOffset(96)]
-        public Vector4 avp; //angular velocity of player in world coordinates
-        //[FieldOffset(100)]
-        public Vector4 aiw; //acceleration of object in world coordinates
-        //[FieldOffset(104)]
-        public System.Single speed; //speed of player;
-        //[FieldOffset(105)]
-        public System.Single spdOfLight; //current speed of light
+        //[FieldOffset(44)]
+        public float speed; //speed of player;
+        //[FieldOffset(45)]
+        public float spdOfLight; //current speed of light
+        //[FieldOffset(46)]
+        public Vector4 pap;
+        //[FieldOffset(50)]
+        public Vector4 avp;
+        //[FieldOffset(52)]
+        public Vector4 aiw;
+        //[FieldOffset(56)]
+        public Matrix4x4 vpcLorentzMatrix;
+        //[FieldOffset(72)]
+        public Matrix4x4 viwLorentzMatrix;
+        //[FieldOffset(88)]
     }
 }
