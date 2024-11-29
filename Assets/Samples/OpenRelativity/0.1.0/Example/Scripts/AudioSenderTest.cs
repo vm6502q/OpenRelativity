ca0bfa72ec8eb3d55fd85a21502994035215b611
WrathfulSpatula <stranoj@gmail.com>
2024-11-29 16:24:46 -0500

    Proper Samples~

---
+++ Assets/Samples/OpenRelativity/0.1.0/Example/Scripts/AudioSenderTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using OpenRelativity;
+
+namespace OpenRelativity.Samples {
+public class AudioSenderTest : MonoBehaviour
+{
+    private RelativisticObject myRO;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        myRO = GetComponent<RelativisticObject>();
+    }
+
+    // Update is called once per frame
+    protected void FixedUpdate()
+    {
+        myRO.viw = (1 - 0.01f * Time.fixedDeltaTime) * myRO.viw;
+    }
+}
+}
